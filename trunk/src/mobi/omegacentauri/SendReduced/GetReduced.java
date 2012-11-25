1fd0a07c3303156b393dd84499ebbc6f99330ad1
arpruss@gmail.com <arpruss@gmail.com@bf8ad1b6-8541-5127-a71b-33f59ce61af4>
2012-11-25 21:24:01 +0000

    0.02

--- trunk/src/mobi/omegacentauri/SendReduced/GetReduced.java
+++ trunk/src/mobi/omegacentauri/SendReduced/GetReduced.java
@@ -1,43 +1,9 @@
 package mobi.omegacentauri.SendReduced;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.graphics.Rect;
-import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.Preference;
-import android.preference.PreferenceManager;
-import android.provider.MediaStore;
-import android.text.Html;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 public class GetReduced extends Activity {
 	private static final int REQUEST_LOAD_IMAGE = 1;
--- trunk/src/mobi/omegacentauri/SendReduced/Options.java
+++ trunk/src/mobi/omegacentauri/SendReduced/Options.java
@@ -1,16 +1,7 @@
 package mobi.omegacentauri.SendReduced;
 
-import mobi.omegacentauri.SendReduced.R;
-import android.app.Activity;
-import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
-import android.util.Log;
 
 public class Options extends PreferenceActivity {
 	public static final String PREF_RESOLUTION = "resolution";
