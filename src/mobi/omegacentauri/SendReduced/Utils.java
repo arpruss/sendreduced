14c1bd2582b6f5ba447249071d9f7f776eb17ef3
arpruss@gmail.com <arpruss@gmail.com@bf8ad1b6-8541-5127-a71b-33f59ce61af4>
2012-11-30 01:14:55 +0000

    0.03

--- src/mobi/omegacentauri/SendReduced/Utils.java
+++ src/mobi/omegacentauri/SendReduced/Utils.java
@@ -3,6 +3,7 @@ package mobi.omegacentauri.SendReduced;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -26,18 +28,20 @@ import android.provider.MediaStore;
 public class Utils {
 	static final int BUFSIZE = 16384;
 	static final int INVALID_ROTATION = -360000;
+	static final String PREFIX = "Image";
+	static final long CLEAN_TIME = (2 * 86400l * 1000l);
 	
 	public static boolean sendReduced(Activity a, Uri uri) {
 		ContentResolver cr = a.getContentResolver();
 		SharedPreferences options = PreferenceManager.getDefaultSharedPreferences(a);
 		int resolution =  Integer.parseInt(options.getString(Options.PREF_RESOLUTION, "1024"));
-		Bitmap bmp = getReduced(cr, uri, resolution);
-		if (bmp == null)
+		ReducedImage image = new ReducedImage(cr, uri, resolution);
+		if (image.bmp == null)
 			return false;
-		SendReduced.log("Reduced to "+bmp.getWidth()+"x"+bmp.getHeight());
+		SendReduced.log("Reduced to "+image.bmp.getWidth()+"x"+image.bmp.getHeight());
 		int quality = Integer.parseInt(options.getString(Options.PREF_QUALITY, "85"));
 		SendReduced.log("JPEG compression quality "+quality);
-		String path = saveImage(a, bmp, quality);
+		String path = saveImage(a, image.bmp, quality);
 		if (path == null) 
 			return false;
 		Intent i = new Intent(android.content.Intent.ACTION_SEND);
@@ -47,15 +51,23 @@ public class Utils {
 		return true;
 	}
 	
-	private static String saveImage(Context c, Bitmap bmp, int compress) {
+	private static File getCacheDir(Context c) {
 		File storage;
 		if (Build.VERSION.SDK_INT >= 8)
 			storage = c.getExternalCacheDir();
 		else
 			storage = new File(Environment.getExternalStorageDirectory().getPath()+"/SendReduced");
 		storage.mkdir();
+		return storage;
+	}
+	
+	private static String saveImage(Context c, Bitmap bmp, int compress) {
+		cleanCache(c);
+		
+		File storage = getCacheDir(c);
+		
 		try {
-			File temp = File.createTempFile("Image", ".jpg", storage);
+			File temp = File.createTempFile(PREFIX, ".jpg", storage);
 			SendReduced.log("Compressing to "+temp);
 			FileOutputStream out = new FileOutputStream(temp);
 			boolean status = bmp.compress(Bitmap.CompressFormat.JPEG, compress, out);
@@ -71,60 +83,26 @@ public class Utils {
 		}
 		
 	}
-
-	public static Bitmap getReduced(ContentResolver cr, Uri uri, int resolution) {
-		SendReduced.log("Reducing "+uri);
-		byte[] data;
-		try {
-			data = getStreamBytes(cr.openInputStream(uri));
-		} catch (FileNotFoundException e) {
-			SendReduced.log("error reading: "+e);
-			return null;
-		}
-		
-		if (data == null) 
-			return null;
-		
-		SendReduced.log("need to decode "+data.length+" bytes");
-
-		int o = getOrientation(cr, uri);
-		SendReduced.log("orientation = "+o);
-		
-		Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
-		
-		if (bmp == null) {
-			SendReduced.log("error decoding");
-			return null;
-		}
-		
-		int h = bmp.getHeight();
-		int w = bmp.getWidth();
-		
-		boolean transform = false;
-		
-		Matrix m = new Matrix();
-		
-		if (h > resolution || w > resolution) {
-			if (h>w)
-				m.postScale(resolution / (float)h, resolution / (float)h);
-			else
-				m.postScale(resolution / (float)w, resolution / (float)w);
-			transform = true;
-		}
-		
-		if (INVALID_ROTATION != o) {
-			m.postRotate(o);
-			transform = true;
-		}
-				
-		SendReduced.log("image: "+w+"x"+h+" ["+o+"]");
-
-		if (transform)
-			return Bitmap.createBitmap(bmp, 0, 0, w, h, m, true);
-		else
-			return bmp;
-	}
 	
+	public static void cleanCache(Context c) {		
+		File[] list = getCacheDir(c).listFiles(new FilenameFilter(){
+
+			@Override
+			public boolean accept(File dir, String filename) {
+				return filename.toLowerCase().endsWith(".jpg") && 
+					filename.toLowerCase().startsWith(PREFIX.toLowerCase());
+			}});
+		
+		long t = System.currentTimeMillis();
+		for (File f: list) {			
+			if (f.lastModified() < t - CLEAN_TIME ||
+					t + CLEAN_TIME < f.lastModified()) {
+				SendReduced.log("cleaning up "+f);
+				f.delete();
+			}
+		}
+	}
+
 	static private int getOrientation(ContentResolver cr, Uri uri) {
 		Cursor c = 
 			android.provider.MediaStore.Images.Media.query(cr, uri, 
@@ -184,17 +162,82 @@ public class Utils {
 		SendReduced.log("orientation: "+orientation);
 		SharedPreferences options = PreferenceManager.getDefaultSharedPreferences(a);
 		int resolution =  Integer.parseInt(options.getString(Options.PREF_RESOLUTION, "1024"));
-		Bitmap bmp = getReduced(cr, uriIn, resolution);
-		if (bmp == null)
+		ReducedImage image = new ReducedImage(cr, uriIn, resolution);
+		if (image.bmp == null)
 			return null;
-		SendReduced.log("Reduced to "+bmp.getWidth()+"x"+bmp.getHeight());
+		SendReduced.log("Reduced to "+image.bmp.getWidth()+"x"+image.bmp.getHeight());
 		int quality = Integer.parseInt(options.getString(Options.PREF_QUALITY, "85"));
 		SendReduced.log("JPEG compression quality "+quality);
-		String path = saveImage(a, bmp, quality);
+		String path = saveImage(a, image.bmp, quality);
 		if (path == null) 
 			return null;
 		else
 			return Uri.fromFile(new File(path));
 	}
-	
+
+	static class ReducedImage {
+		long date;
+		Bitmap bmp;
+		
+		public ReducedImage(ContentResolver cr, Uri uri, int resolution) {
+			bmp = null;
+			
+			SendReduced.log("Reducing "+uri);
+			byte[] data;
+			try {
+				data = getStreamBytes(cr.openInputStream(uri));
+			} catch (FileNotFoundException e) {
+				SendReduced.log("error reading: "+e);
+				return;
+			}
+			
+			if (data == null) 
+				return;
+			
+			date = -1;
+			if (uri.getScheme().equalsIgnoreCase("file")) {
+				File f = new File(uri.getPath());
+				date = f.lastModified();
+			}
+			
+			SendReduced.log("need to decode "+data.length+" bytes");
+
+			int o = getOrientation(cr, uri);
+			SendReduced.log("orientation = "+o);
+			
+			Bitmap inBmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+			
+			if (inBmp == null) {
+				SendReduced.log("error decoding");
+				return;
+			}
+			
+			int h = inBmp.getHeight();
+			int w = inBmp.getWidth();
+			
+			boolean transform = false;
+			
+			Matrix m = new Matrix();
+			
+			if (h > resolution || w > resolution) {
+				if (h>w)
+					m.postScale(resolution / (float)h, resolution / (float)h);
+				else
+					m.postScale(resolution / (float)w, resolution / (float)w);
+				transform = true;
+			}
+			
+			if (INVALID_ROTATION != o) {
+				m.postRotate(o);
+				transform = true;
+			}
+					
+			SendReduced.log("image: "+w+"x"+h+" ["+o+"]");
+
+			if (transform)
+				bmp = Bitmap.createBitmap(inBmp, 0, 0, w, h, m, true);
+			else
+				bmp = inBmp;
+		}
+	}
 }
