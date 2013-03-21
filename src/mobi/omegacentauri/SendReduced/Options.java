57541199476fc51d0a625e11615916e0c9e8eca9
arpruss@gmail.com <arpruss@gmail.com@bf8ad1b6-8541-5127-a71b-33f59ce61af4>
2013-03-21 02:25:29 +0000

    support SEND_MULTIPLE

--- src/mobi/omegacentauri/SendReduced/Options.java
+++ src/mobi/omegacentauri/SendReduced/Options.java
@@ -6,6 +6,11 @@ import android.preference.PreferenceActivity;
 public class Options extends PreferenceActivity {
 	public static final String PREF_RESOLUTION = "resolution";
 	public static final String PREF_QUALITY = "quality";
+	public static final String PREF_NAME = "name";
+	public static final String OPT_NAME_DATE_TIME = "date and time";
+	public static final String OPT_NAME_RANDOM = "random";
+	public static final String OPT_NAME_SEQUENTIAL = "sequential";
+	public static final String PREF_SEQUENCE_POSITION = "sequencePosition";
 	
 	@Override
 	public void onCreate(Bundle icicle) {
