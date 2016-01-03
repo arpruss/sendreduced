1e94cc0d8d14bd783125402b06ae425f7f620c9f
arpruss <arpruss@gmail.com>
2016-01-02 21:27:39 -0600

    restore multiple send code

--- src/mobi/omegacentauri/SendReduced/SendReduced.java
+++ src/mobi/omegacentauri/SendReduced/SendReduced.java
@@ -1,28 +1,33 @@
 package mobi.omegacentauri.SendReduced;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
 public class SendReduced extends Activity {
 	private static boolean DEBUG = true;
-	
+
 	public static void log(String s) {
 		if (DEBUG )
 			Log.v("SendReduced", s);
 	}
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		SharedPreferences options = PreferenceManager.getDefaultSharedPreferences(this);
+
 		Intent i = getIntent();
 		Bundle e = i.getExtras();
 		if (e != null && e.containsKey(Utils.INTENT_FROM_ME)) {
@@ -34,11 +39,38 @@ public class SendReduced extends Activity {
 		}
 		if (i.getAction().equals(Intent.ACTION_SEND)) {
 			if (e != null &&
-				e.containsKey(Intent.EXTRA_STREAM))  {
+					e.containsKey(Intent.EXTRA_STREAM))  {
 				new Utils(this).sendReduced(
 						(Uri)e.getParcelable(Intent.EXTRA_STREAM));
 			}
 			finish();
 		}
-    }
+		else if (i.getAction().equals(Intent.ACTION_SEND_MULTIPLE)) {
+			if (e != null &&
+					e.containsKey(Intent.EXTRA_STREAM)) {
+				ArrayList<Uri> in = e.getParcelableArrayList(Intent.EXTRA_STREAM);
+				ArrayList<Uri> out = new ArrayList<Uri>();
+				Utils utils = new Utils(this);
+
+				for (Uri uri: in) {
+					Uri reduced = utils.reduce(uri);
+					if (reduced != null)
+						out.add(reduced);
+				}
+				
+				if (out.size()>0) {
+					if (! options.getString(Options.PREF_NAME, Options.OPT_NAME_RANDOM).equals(Options.OPT_NAME_RANDOM))
+						Collections.sort(out);
+					
+					Intent go = new Intent(android.content.Intent.ACTION_SEND_MULTIPLE);
+					go.setType("text/plain");
+					go.putParcelableArrayListExtra(android.content.Intent.EXTRA_STREAM, out);
+					startActivity(go);
+				}
+			}
+		}
+		else {
+			finish();
+		}
+	}
 }
